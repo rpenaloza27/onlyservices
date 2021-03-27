bc5b7da1d6cada076659a31d161315e02c813397
Roibert Penaloza <rpenaloza@estrategiaswebcolombia.com>
2021-03-27 11:04:03 -0500

    Added

--- server.js
+++ server.js
@@ -30,21 +30,21 @@ const routes = require("./app/routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
-const https = require("https");
-const fs = require("fs");
-const options = { 
-  key: fs.readFileSync("my_cert.key"),
-  cert: fs.readFileSync("my_cert.crt")
-}
-const http= require("http");
-http.createServer(app).listen(PORT);
+// const https = require("https");
+// const fs = require("fs");
+// const options = { 
+//   key: fs.readFileSync("my_cert.key"),
+//   cert: fs.readFileSync("my_cert.crt")
+// }
+// const http= require("http");
+// http.createServer(app).listen(PORT);
 
-https.createServer(options,app).listen(443,()=>{
-  console.log("My Server is running")
-})
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}.`);
-// });
+// https.createServer(options,app).listen(443,()=>{
+//   console.log("My Server is running")
+// })
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 
 const db = require("./app/models");
 try{
